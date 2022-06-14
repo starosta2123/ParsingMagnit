a72c7360274c2cb77f08e633ae601d2566bd7019
starosta2123 <[admin@mbou69.com]>
2022-06-14 16:30:29 +0400

    #1 Comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def collect_data(city_code='2209'):
     cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
     ua = UserAgent()
 
-#Словарь для заголовков
+#Heading Dictionary
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'User-Agent': ua.random
@@ -18,11 +18,11 @@ def collect_data(city_code='2209'):
         'mg_geo_id': f'{city_code}'
     }
 
-#Отправляем запрос на сайт
+#Sending a request to the site
 
     response = requests.get(url='https://magnit.ru/promo/', headers=headers, cookies=cookies)
 
-#Сохраняем результат
+#Save the result
     with open(f'file.html', 'w', encoding="UTF-8") as file:
         file.write(response.text)
 
